3814cfbe4c884ce84d5bdadb7506c2b622029585
GabrielFinkler <110740313+GabrielFinkler@users.noreply.github.com>
2025-09-15 23:00:39 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -6,68 +6,71 @@ int main(void)
     float pNumero, sNumero, resultado;
     char o;
 
-    printf("Selecione a operacao a ser realizadas: \n");
-    printf("+ : Soma \n");
-    printf("- : Subtracao \n");
-    printf("* : Multiplicacao \n");
-    printf("/ : Divisao \n");
-    printf("P : Potenciacao \n");
-    printf("R : Raiz quadrada \n");
-    scanf("%c", &o);
-
-    switch(o)
+    while ( o != '0')
     {
-        case '+' :
-            printf("Digite o primeiro numero: ");
-            scanf("%f", &pNumero);
-            printf("Digite o segundo numero: ");
-            scanf("%f", &sNumero);
-            resultado = pNumero + sNumero;
-            printf("%f + %f = %f", pNumero, sNumero, resultado);
-            break;
-        case '-' :
-            printf("Digite o primeiro numero: ");
-            scanf("%f", &pNumero);
-            printf("Digite o segundo numero: ");
-            scanf("%f", &sNumero);
-            resultado = pNumero - sNumero;
-            printf("%.2f - %.2f = %.2f", pNumero, sNumero, resultado);
-            break;
-        case '*' :
-            printf("Digite o primeiro numero: ");
-            scanf("%f", &pNumero);
-            printf("Digite o segundo numero: ");
-            scanf("%f", &sNumero);
-            resultado = pNumero * sNumero;
-            printf("%.2f * %.2f = %.2f", pNumero, sNumero, resultado);
-            break;
-        case '/' :
-            printf("Digite o primeiro numero: ");
-            scanf("%f", &pNumero);
-            printf("Digite o segundo numero: ");
-            scanf("%f", &sNumero);
-            resultado = pNumero / sNumero;
-            printf("%.2f / %.2f = %.2f", pNumero, sNumero, resultado);
-            break;
-        case 'P':
-            printf("Digite o primeiro numero: ");
-            scanf("%f", &pNumero);
-            printf("Digite o segundo numero: ");
-            scanf("%f", &sNumero);
-            resultado = pow(pNumero,sNumero);
-            printf("%.2f ** %.2f = %.2f", pNumero, sNumero, resultado);
-            break;
-        case 'R':
-            printf("Digite o numero: ");
-            scanf("%f", &pNumero);
-            resultado = sqrt(pNumero);
-            printf("A raiz de %.2f e %.2f.", pNumero, resultado);
-            break;
-        default:
-            printf("Operacao invalida");
-            break;
-
+        printf("Selecione a operacao a ser realizadas: \n");
+        printf("+ : Soma \n");
+        printf("- : Subtracao \n");
+        printf("* : Multiplicacao \n");
+        printf("/ : Divisao \n");
+        printf("P : Potenciacao \n");
+        printf("R : Raiz quadrada \n");
+        printf("0 : Finalizar \n");
+        scanf(" %c", &o);
+        switch(o)
+        {
+            case '+' :
+                printf("Digite o primeiro numero: ");
+                scanf("%f", &pNumero);
+                printf("Digite o segundo numero: ");
+                scanf("%f", &sNumero);
+                resultado = pNumero + sNumero;
+                printf("%.2f + %.2f = %.2f \n", pNumero, sNumero, resultado);
+                break;
+            case '-' :
+                printf("Digite o primeiro numero: ");
+                scanf("%f", &pNumero);
+                printf("Digite o segundo numero: ");
+                scanf("%f", &sNumero);
+                resultado = pNumero - sNumero;
+                printf("%.2f - %.2f = %.2f \n", pNumero, sNumero, resultado);
+                break;
+            case '*' :
+                printf("Digite o primeiro numero: ");
+                scanf("%f", &pNumero);
+                printf("Digite o segundo numero: ");
+                scanf("%f", &sNumero);
+                resultado = pNumero * sNumero;
+                printf("%.2f * %.2f = %.2f \n", pNumero, sNumero, resultado);
+                break;
+            case '/' :
+                printf("Digite o primeiro numero: ");
+                scanf("%f", &pNumero);
+                printf("Digite o segundo numero: ");
+                scanf("%f", &sNumero);
+                resultado = pNumero / sNumero;
+                printf("%.2f / %.2f = %.2f \n", pNumero, sNumero, resultado);
+                break;
+            case 'P':
+                printf("Digite o primeiro numero: ");
+                scanf("%f", &pNumero);
+                printf("Digite o segundo numero: ");
+                scanf("%f", &sNumero);
+                resultado = pow(pNumero,sNumero);
+                printf("%.2f ** %.2f = %.2f \n", pNumero, sNumero, resultado);
+                break;
+            case 'R':
+                printf("Digite o numero: ");
+                scanf("%f", &pNumero);
+                resultado = sqrt(pNumero);
+                printf("A raiz de %.2f e %.2f. \n", pNumero, resultado);
+                break;
+            default:
+                printf("Operacao invalida \n");
+                break;
+        }
 
     }
+    printf("Calculadora finalizada");
     return 0;
 }
